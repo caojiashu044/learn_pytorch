78a21984fb8121e3f90742e1fc5c9a92f100b03d
caojiashu044 <caojiashu044@gmail.com>
2026-06-10 21:41:37 +0800

    121

--- read_data.py
+++ read_data.py
@@ -1,1 +1,11 @@
-print("hello world")
+from torch.utils.data import Dataset
+import cv2
+from PIL import Image
+# class MyData(Dataset):
+
+#     def __init__(self):
+
+#     def getitem__(self, idx):
+img_path = "./hymenoptera_data/train/ants/0013035.jpg"
+
+img = Image.open(img_path)
